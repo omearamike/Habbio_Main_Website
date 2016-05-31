dba4aeefe9369fa388346fa31025140a6080b17c
omearamike <mikeytoomo@hotmail.com>
2016-05-31 13:31:41 +0100

    - Profile Page done with tabs

--- wp-content/themes/jax-lite/footer.php
+++ wp-content/themes/jax-lite/footer.php
@@ -1,13 +1,35 @@
 		<?php do_action('jaxlite_footer_sidebar'); ?>
-        
-        <div id="back-to-top"> <i class="fa fa-chevron-up"></i> </div>
-    
+
+        <div id="back-to-top"> <i class="fa fa-chevron-up up_button"></i> </div>
+
     </div>
-    
+
 </div>
 
-<?php wp_footer() ?>  
- 
+<div class="mainFooter">
+	<div class="footerContent">
+
+		<div class="topFooter">
+				<a href="#" class="downloadBtn">Get Habi Now</a>
+			</div>
+
+		<div class="footerLeft">
+		</div>
+
+		<div class="footerCenter">
+		</div>
+
+		<div class="footerRight">
+		</div>
+	</div>
+
+</div>
+
+<?php wp_footer() ?>
+
+
+
+
 </body>
 
 </html>
--- wp-content/themes/jax-lite/header.php
+++ wp-content/themes/jax-lite/header.php
@@ -8,6 +8,7 @@
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="http://localhost:8888/Habiio/wp-content/themes/jax-lite/assets/js/customJavascript.js"></script>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.2, user-scalable=yes" />
 
